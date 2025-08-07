ca60934d8988953ab378f798a35d38c90c912b77
Dina <andrianambinintsoadina@Gmail.com>
2025-08-07 21:04:30 +0300

    adding all modified files

---
+++ file.R
@@ -0,0 +1,60 @@
+#library importation
+library("tidync")
+library("dplyr")
+library("ggplot2")
+#dataset importation
+dataset2 = tidync("/media/dina/f4c07323-3819-4c76-ad53-95f7d45b7ae2/morondava/dataset2.nc")
+dataset = tidync("/media/dina/f4c07323-3819-4c76-ad53-95f7d45b7ae2/morondava/dataset.nc")
+#temperature data and correction data extraction
+modification = dataset%>%hyper_array()
+temperature = dataset2%>%hyper_array()
+temperature = tibble((temperature$t2m+modification$tp)-273.15)
+#plotting
+data = data.frame(years = 1962:2025,temperature = temperature)
+plot = ggplot(data = data)+
+  geom_line(mapping = aes(x = years,y = X.temperature.t2m...modification.tp....273.15))+
+  geom_point(mapping = aes(x = years,y = X.temperature.t2m...modification.tp....273.15),color = "blue")+
+  geom_smooth(mapping = aes(x = years,y = X.temperature.t2m...modification.tp....273.15))+
+  labs(title = "Annual temperature of September 22 for Morondava (1962-2025)",x = "Years",y = "temperature[°C]")
+
+print(plot)
+#descriptive statistics
+period_1 = data$X.temperature.t2m...modification.tp....273.15[1:32]
+period_2 = data$X.temperature.t2m...modification.tp....273.15[32:64]
+mean = mean(data$X.temperature.t2m...modification.tp....273.15)
+sdt = sqrt(var(data$X.temperature.t2m...modification.tp....273.15))
+p1_mean = mean(period_1)
+p1_std = sqrt(var(period_1))
+p2_mean = mean(period_2)
+p2_std = sqrt(var(period_2))
+print(mean)
+max = max(data$X.temperature.t2m...modification.tp....273.15)
+min = min(data$X.temperature.t2m...modification.tp....273.15)
+print(max)
+print(min)
+print(sdt)
+print(p1_mean)
+print(p2_mean)
+#inferential statistics for a non exhaustive sample with significant level = 0.01
+u_alpha = 0.01
+tou_alpha = 2.33
+#getting estimation interval
+mean_interval = function(x,y,n){
+  A = x - u_alpha*(y/sqrt(n - 1))
+  B = x + u_alpha*(y/sqrt(n-1))
+  return (c(A,B))
+}
+print(mean_interval(mean,std,length(data$X.temperature.t2m...modification.tp....273.15)))
+#searching if the first period is warmer than second period
+test = function(x1,x2,sigma1,sigma2,n1,n2){
+  T = abs(x1 - x2)/sqrt((sigma1**2/(n1 - 1))+(sigma2**2/(n2 - 1)))
+  if (T <= tou_alpha){
+    print(T)
+    return("The mean temperature of the two periods are the same")
+  }
+  else{
+    print(T)
+    return("The temperature of the second period is higher")
+  }
+}
+print(test(p1_mean,p2_mean,p1_std,p2_std,length(period_1),length(period_2)))
